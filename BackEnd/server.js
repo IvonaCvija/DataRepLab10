e5b1675ca305e78e1e286fae331c2193815765ab
Ivona Cvija <ivca.cvija@gmail.com>
2023-12-08 11:35:14 +0000

    lab10

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,14 +3,21 @@ const app = express()
 const port = 4000;
 const cors = require('cors');
 
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+//server.js
+//add just under import section at the top of server,js
+// Serve the static files from the React app
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
+// app.use(cors());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -76,12 +83,18 @@ app.get(`/api/book/:id`, async (req, res) => {
     res.send(book);
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
 app.get('/api/books', async (req, res) => {
 
     let books = await bookModel.find({});
     res.json(books);
+})
+
+//add at the bottom just over app.listen
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../build/index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
